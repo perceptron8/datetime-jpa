25471b89cf09246a4dec21e67cd562fa0e2d2a74
perceptron8 <michal@top-team.pl>
2015-01-13 21:18:06 +0100

    DayOfWeekConverter + test

---
+++ src/main/java/com/github/perceptron8/datetime/jpa/DayOfWeekConverter.java
@@ -0,0 +1,23 @@
+package com.github.perceptron8.datetime.jpa;
+
+import java.sql.Date;
+import java.time.DayOfWeek;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * Converts {@link DayOfWeek} to {@link Date} and back again.
+ */
+@Converter(autoApply = true)
+public class DayOfWeekConverter implements AttributeConverter<DayOfWeek, Integer> {
+	@Override
+	public Integer convertToDatabaseColumn(DayOfWeek attribute) {
+		return attribute == null ? null : attribute.getValue();
+	}
+
+	@Override
+	public DayOfWeek convertToEntityAttribute(Integer dbData) {
+		return dbData == null ? null : DayOfWeek.of(dbData);
+	}
+}
---
+++ src/test/java/com/github/perceptron8/datetime/jpa/DayOfWeekConverterTest.java
@@ -0,0 +1,33 @@
+package com.github.perceptron8.datetime.jpa;
+
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+import java.time.DayOfWeek;
+
+import javax.persistence.AttributeConverter;
+
+import org.junit.Test;
+
+public class DayOfWeekConverterTest {
+	private AttributeConverter<DayOfWeek, Integer> converter = new DayOfWeekConverter();
+
+	@Test
+	public void convertToDatabaseColumn() {
+		assertThat(converter.convertToDatabaseColumn(null), is(nullValue()));
+		for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+			assertThat(converter.convertToDatabaseColumn(dayOfWeek), is(equalTo(dayOfWeek.getValue())));
+		}
+	}
+
+	@Test
+	public void convertToEntityAttribute() {
+		assertThat(converter.convertToEntityAttribute(null), is(nullValue()));
+		for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+			assertThat(converter.convertToEntityAttribute(dayOfWeek.getValue()), is(equalTo(dayOfWeek)));
+		}
+	}
+}
